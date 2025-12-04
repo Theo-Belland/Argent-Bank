dbd16109e315ed3083d8bbd032099e7cc1c992fb
Theo <theo.belland.pro@gmail.com>
2025-12-04 10:54:29 +0100

    modification du responsive.

--- Front-End/src/components/Header.jsx
+++ Front-End/src/components/Header.jsx
@@ -23,44 +23,48 @@ export default function Header() {
 
   return (
     <nav className="main-nav">
-      <Link className="main-nav-logo" to="/" aria-label="Go To Home">
-        <img
-          className="main-nav-logo-image"
-          src={logo}
-          alt="Argent Bank Logo"
-        />
-        <h1 className="sr-only">Argent Bank</h1>
-      </Link>
+  <Link className="main-nav-logo" to="/" aria-label="Go To Home">
+    <img
+      className="main-nav-logo-image"
+      src={logo}
+      alt="Argent Bank Logo"
+    />
+    <h1 className="sr-only">Argent Bank</h1>
+  </Link>
 
-      <div>
-        {token ? (
-          <>
-            <NavLink
-              className={linkClass}
-              to="/profil"
-              aria-label="Profile"
-            >
-              {displayName} <i className="fa fa-user-circle" />
-            </NavLink>
-            <button
-              type="button"
-              className="main-nav-item button"
-              onClick={onLogout}
-              aria-label="Sign Out"
-            >
-              <i className="fa fa-power-off" />
-            </button>
-          </>
-        ) : (
-          <NavLink
-            className={linkClass}
-            to="/sign-in"
-            aria-label="Sign In"
-          >
-            <i className="fa fa-user-circle" /> Sign In
-          </NavLink>
-        )}
-      </div>
-    </nav>
+  <div>
+    {token ? (
+      <>
+        <NavLink
+          className={linkClass}
+          to="/profile"
+          aria-label="Profile"
+        >
+          <i className="fa fa-user-circle"></i>
+          {displayName}
+        </NavLink>
+
+        <button
+          type="button"
+          className="main-nav-item"
+          onClick={onLogout}
+          aria-label="Sign Out"
+        >
+          <i className="fa fa-sign-out"></i>
+          Sign Out
+        </button>
+      </>
+    ) : (
+      <NavLink
+        className={linkClass}
+        to="/sign-in"
+        aria-label="Sign In"
+      >
+        <i className="fa fa-user-circle"></i>
+        Sign In
+      </NavLink>
+    )}
+  </div>
+</nav>
   )
 }
